77c7b0e56376552e291228357e194fbe51d698df
itsmesroy <itsmevicky78@gmail.com>
2025-01-09 18:27:51 +0530

    Did the first 2 divs Income and expense

--- src/App.js
+++ src/App.js
@@ -1,46 +1,94 @@
-
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [walletBalance, setWalletBalance] = useState(5000);
+  const [expenses, setExpenses] = useState([]);
+  const [isExpenseModalOpen, setIsExpenseModalOpen] = useState(false);
+  const [isIncomeModalOpen, setIsIncomeModalOpen] = useState(false);
+  const [isDeleteExpenseModalOpen, setIsDeleteExpenseModalOpen] =
+    useState(false);
+  const [expenseToEdit, setExpenseToEdit] = useState([]);
+  const [expenseToDelete, setExpenseToDelete] = useState([]);
 
-const [walletBalamce, setWalletBalance]=useState(5000);
-const [expenses, setExpenses]=useState([])
-const [isExpenseModalOpen, setIsExpenseModalOpen]=useState(false);
-const [isIncomeModalOpen, setIsIncomeModalOpen]=useState(false);
-const [isDeleteExpenseModalOpen, setIsDeleteExpenseModalOpen]=useState(false);
-const [expenseToEdit, setExpenseToEdit]=useState([]);
-const [expenseToDelete, setExpenseToDelete]=useState([]);
+  const openIncomeModal = () => setIsIncomeModalOpen(true);
+  const closeIncomeModal = () => setIsIncomeModalOpen(false);
 
-const openIncomeModal=()=> setIsIncomeModalOpen(true);
-const closeIncomeModal=()=> setIsIncomeModalOpen(false);
+  const openExpenseModal = () => setIsExpenseModalOpen(true);
+  const closeExpenseModal = () => setIsExpenseModalOpen(false);
 
-const openExpenseModal=()=> setIsExpenseModalOpen(true);
-const closeExpenseModal=()=> setIsExpenseModalOpen(false);
-
-return (
+  const openEditExpenseModal = (expense) => {
+    setExpenseToEdit(expense);
+    openExpenseModal();
+  };
+  const closeEditExpenseModal = () => {
+    closeExpenseModal();
+    setExpenseToEdit(null);
+  };
+  const openDeleteExpenseModal = (expenseId) => {
+    setIsDeleteExpenseModalOpen(true);
+    setExpenseToDelete(expenseId);
+  };
+  const closeDeleteExpenseModal = () => {
+    setIsDeleteExpenseModalOpen(false);
+  };
+  const addIncome = (amount) => {
+    const newWalletBalance = walletBalance + amount;
+    setWalletBalance(newWalletBalance);
+    localStorage.setItem("WalletBalance", newWalletBalance);
+  };
+  const editIncome = () => {};
+  return (
     <div className="App">
-      <div className='Container'>
+      <div className="Container">
         <h2>Expense Tracker</h2>
 
-        <div className='row align-items-center '>
+        <div className="row align-items-center ">
+          <div className="col-md-6 text-start">
+            <div className="wallet-balance">
+              <h2>
+                <div>Wallet Balance:</div> ₹ {walletBalance}
+              </h2>
+              <button
+                className="button"
+                type="button"
+                style={{
+                  background:
+                    "linear-gradient(90deg, #B5DC52 0%, #89E148 100%)",
+                  color: "#fff",
+                  border: "none",
+                  padding: "10px 20px",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  fontWeight: "bold",
+                }}
+                onClick={openIncomeModal} >
+                +Add Income
+              </button>
+            </div>
+          </div>
 
-        <div className='col-md-6 text-start'>
-        <div className='wallet-balance'>
-          <h1>Wallet Balance:</h1>
-          <button className='button' style={{backgroundColor:"#7fff00"}}>+ Add Income</button>
-        </div>
-        </div>
-        
-        <div className='col-md-6 text-start'>
-        <div className='add-expense'>
-          <h1>Expenses:</h1>
-          <button className='button' style={{backgroundColor:"#ff0028"}}>+ Add Expense</button>
-        </div>
-        </div>
+          <div className="col-md-6 text-start">
+            <div className="add-expense">
+              <h1>Expenses:</h1>
+              <button type="button" onClick={openExpenseModal}style={{  background:
+                   "linear-gradient(90deg, #FF9595 0%, #FF4747 80%, #FF3838 100%)",
+                    color: "#fff",
+                    border: "none",
+                    padding: "10px 20px",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                    fontWeight: "bold",}}>
+                +Add Expense
+              </button>
+            </div>
+          </div>
+
 
         </div>
       </div>
+      <IncomeModal/>
+      <ExpenseModal/>
     </div>
   );
 }
